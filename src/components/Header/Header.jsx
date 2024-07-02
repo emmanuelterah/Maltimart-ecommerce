3852a376351c00081305ef51df7467032cdd1622
Emmanuel Terah <emmanuelterah98@gmail.com>
2024-07-02 22:55:50 +0300

    responsive landing page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -29,6 +29,8 @@ const Header = () => {
 
   const headerRef = useRef(null);
 
+  const menuRef = useRef(null);
+
   const stickyHeaderFunc = () => {
     window.addEventListener('scroll', ()=> {
       if(document.body.scrollTop > 80 || 
@@ -47,6 +49,8 @@ const Header = () => {
       return ()=> window.removeEventListener('scroll', stickyHeaderFunc)
   })
 
+  const menuToggle = () => menuRef.current.classList.toggle('active__menu')
+
   return (
     <header className='header' ref={headerRef}>
       <Container>
@@ -59,7 +63,7 @@ const Header = () => {
               </div>
             </div>
 
-            <div className="navigation">
+            <div className="navigation" ref={menuRef} onClick={menuToggle}>
               <ul className="menu">
                 {
                   nav__links.map((item, index)=>(
@@ -85,13 +89,14 @@ const Header = () => {
               <span>
                 <motion.img whileTap={{scale: 1.2 }} src={user_icon} alt=""/>
               </span>
+              <div className="mobile__menu">
+                <span onClick={menuToggle}>
+                  <i class="ri-menu-line"></i>
+                </span>
+              </div>
             </div>
 
-            <div className="mobile__menu">
-              <span>
-                <i class="ri-menu-line"></i>
-              </span>
-            </div>
+            
           </div>
         </Row>
       </Container>
