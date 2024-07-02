6be18c353e64c63b4fe19859a49335c8c144e43e
Emmanuel Terah <emmanuelterah98@gmail.com>
2024-07-02 20:01:40 +0300

    added best selling section and trending section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,6 +20,9 @@ const Home = () => {
 
   const [trendingProducts, setTrendingProducts] = useState([])
   const [bestSalesProducts, setBestSalesProducts] = useState([])
+  const [mobileProducts, setMobileProducts] = useState([])
+  const [wirelessProducts, setWirelessProducts] = useState([])
+  const [popularProducts, setPopularProducts] = useState([])
 
   const year = new Date().getFullYear()
 
@@ -31,8 +34,24 @@ const Home = () => {
       (item) => item.category === "sofa"
     )
 
+    const filteredMobileProducts = products.filter (
+      (item) => item.category === "mobile"
+    )
+
+    const filteredWirelessProducts = products.filter (
+      (item) => item.category === "wireless" 
+    )
+    
+    const filteredPopularProducts = products.filter (
+      (item) => item.category === "watch" 
+    )
+
     setTrendingProducts(filteredTrendingProducts);
-    setBestSalesProducts(filteredBestSalesProducts)
+    setBestSalesProducts(filteredBestSalesProducts);
+    setMobileProducts(filteredMobileProducts);
+    setWirelessProducts(filteredWirelessProducts);
+    setPopularProducts(filteredPopularProducts);
+
   }, []);
 
   return (
@@ -108,6 +127,31 @@ const Home = () => {
             </Col>
           </Row>
         </Container>
+      </section>
+
+      <section className="new__arrivals">
+        <Container>
+          <Row>
+              <Col lg='12' className="text-center mb-5">
+                <h2 className='section__title'>New Arrivals</h2>
+              </Col>
+
+              <ProductsList data={mobileProducts} />
+              <ProductsList data={wirelessProducts} />
+          </Row>
+        </Container>
+      </section>
+
+      <section className="popular__category">
+        <Container>
+            <Row>
+                <Col lg='12' className="text-center">
+                  <h2 className='section__title'>Popular in Category</h2>
+                </Col>
+
+                <ProductsList data={popularProducts} />
+            </Row>
+          </Container>
       </section>
     </Helmet>
   )
